c7b92bf91e2ed8bf4f7aebd7228529dc9c03d1be
Ayush <ayubantia@gmail.com>
2020-11-13 17:08:59 +0530

    initial commit

---
+++ build/precache-manifest.883993e0fdee6f39b3ebe6acb510685f.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c68c1a736ac2049c7db",
+    "url": "/static/css/main.5cf7ef9e.chunk.css"
+  },
+  {
+    "revision": "7c68c1a736ac2049c7db",
+    "url": "/static/js/main.9b995892.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c598f464a5f425aaa3d1",
+    "url": "/static/css/2.ae81b225.chunk.css"
+  },
+  {
+    "revision": "c598f464a5f425aaa3d1",
+    "url": "/static/js/2.81ac0773.chunk.js"
+  },
+  {
+    "revision": "bb55043a94a2aa3cb8486ce8980f82f2",
+    "url": "/static/media/logo1.bb55043a.png"
+  },
+  {
+    "revision": "b840e171a500c69863f00f6b4056b486",
+    "url": "/static/media/close-24px.b840e171.svg"
+  },
+  {
+    "revision": "0ddcb928ca6b61e0b87b4abb5adbd674",
+    "url": "/static/media/fullwhite5.112.0ddcb928.png"
+  },
+  {
+    "revision": "7b89ccb1f5f963c4c8328fe4bf108565",
+    "url": "/static/media/Spec. sheet for initial customers.7b89ccb1.pdf"
+  },
+  {
+    "revision": "e0794975c05abfe30fffc189a445fedd",
+    "url": "/static/media/speed.e0794975.png"
+  },
+  {
+    "revision": "6a3f14636159808a27265342bf8e2215",
+    "url": "/static/media/battery.6a3f1463.png"
+  },
+  {
+    "revision": "50782e79ef4d5dfed638e4126c2d226e",
+    "url": "/static/media/range white.50782e79.png"
+  },
+  {
+    "revision": "c2d1ddae32f44238bc9798e0636fb76d",
+    "url": "/static/media/middle3.c2d1ddae.png"
+  },
+  {
+    "revision": "db7e017435f7b17cf96837ec71e0e637",
+    "url": "/static/media/throttle2.db7e0174.png"
+  },
+  {
+    "revision": "04a664efc47f73bca3ebf454bc85d0a6",
+    "url": "/static/media/11.04a664ef.png"
+  },
+  {
+    "revision": "4b1e044f74a3c26a9d8d7b6f75a0c012",
+    "url": "/static/media/wheel3.6.4b1e044f.png"
+  },
+  {
+    "revision": "7a19b5241bdeb6a9be319876e1c25659",
+    "url": "/static/media/drivetrain4.7a19b524.png"
+  },
+  {
+    "revision": "2479c40cb73352da92e382a7c97164f7",
+    "url": "/static/media/exploded2.106.2479c40c.png"
+  },
+  {
+    "revision": "e95d259297542c75f3538c7e27019dc1",
+    "url": "/static/media/exploded render 1.39.e95d2592.jpg"
+  },
+  {
+    "revision": "45e301b2768a016bb3ba7d53878d6d54",
+    "url": "/static/media/Sequence 02_2.45e301b2.gif"
+  },
+  {
+    "revision": "686e8d23f1c8964dae4c2bbf3f1c5e46",
+    "url": "/static/media/fullBike3.686e8d23.jpg"
+  },
+  {
+    "revision": "eafafcb98e89df42071d5489e79bb9be",
+    "url": "/static/media/front.eafafcb9.png"
+  },
+  {
+    "revision": "03fbf4bdcfa49f31e98dac89286b9d2f",
+    "url": "/static/media/Bikeimg1.03fbf4bd.jpeg"
+  },
+  {
+    "revision": "e386584bc3f7e9c22f649748016db836",
+    "url": "/static/media/battery.e386584b.jpeg"
+  },
+  {
+    "revision": "4c2d41dd490b9f20fec90f357dd994de",
+    "url": "/static/media/fullwhite4.4c2d41dd.jpg"
+  },
+  {
+    "revision": "6ebd6656acc2a3e995982c18a2ed54cd",
+    "url": "/static/media/fullBike2.6ebd6656.jpg"
+  },
+  {
+    "revision": "5a061e1edcd7af6261fbf6fc67957f8e",
+    "url": "/static/media/fullBike6.5a061e1e.jpeg"
+  },
+  {
+    "revision": "5b0eecfd11399ccba856e342ac2ce5b9",
+    "url": "/static/media/Privacy and Cookie Policy- 09-11-2020.5b0eecfd.pdf"
+  },
+  {
+    "revision": "9b00a5820f99027f4f8cb6214019605a",
+    "url": "/static/media/9.9b00a582.png"
+  },
+  {
+    "revision": "941d008a87190a930573867b8b2c0037",
+    "url": "/static/media/Headache-Font-OTF.941d008a.otf"
+  },
+  {
+    "revision": "a60d20c5e68335aa090c05fbba445407",
+    "url": "/index.html"
+  }
+];
